98524ae3866c5947a6de98794831144eef3256c8
Romain Beauxis <toots@rastageeks.org>
2017-10-23 18:31:06 -0500

    Explicitely use PATH-available cross-compilation, host prefixed binaries.

--- packages/ocaml-windows64.4.04.0/files/install.sh
+++ packages/ocaml-windows64.4.04.0/files/install.sh
@@ -13,6 +13,3 @@ done
 for pkg in bigarray bytes compiler-libs dynlink findlib graphics num num-top stdlib str threads unix; do
   cp -r "${PREFIX}/lib/${pkg}" "${PREFIX}/windows-sysroot/lib/"
 done
-
-mkdir -p "${PREFIX}/lib/findlib.conf.d"
-cp windows.conf "${PREFIX}/lib/findlib.conf.d"
